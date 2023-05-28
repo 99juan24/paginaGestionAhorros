c83bda3c5008159dafb30142ec6ae7a4b13521bc
Juan <juan990424@outlook.com>
2023-05-28 13:05:23 -0500

    crud de gastos terminado

---
+++ Formularios/Gasto.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PaginaAhorro.Formularios
+{
+    public partial class Gasto : System.Web.UI.Page
+    {
+        public ModeloGastosDataContext bd = new ModeloGastosDataContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnEliminar.Attributes.Add("Onclick", "return confirm('Desea eliminar el registro ? ')");
+                btnGuardar.Attributes.Add("Onclick", "return alert('Registro guardado...')");
+                btnModificar.Attributes.Add("Onclick", "return alert('Registro modificado...')");
+                carga();
+            }
+
+        }
+        public void carga()
+        {
+            DatosGastos.DataBind();
+        }
+
+        protected void LinqDataSource1_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            var misdatos = from registros in bd.gasto select registros;
+            e.Arguments.TotalRowCount = misdatos.Count();
+            e.Result = misdatos;
+
+        }
+        public void limpiar()
+        {
+            txtnombre.Text = "";
+            txtmonto.Text = "";
+            txtfechacreacion.Text = "";
+            txfechalimite.Text = "";
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(txtid.Text);
+                String nombre = txtnombre.Text;
+                float cantidad = float.Parse(txtmonto.Text);
+                DateTime fecha_creacion = DateTime.Parse(txtfechacreacion.Text);
+                DateTime fecha_limite = DateTime.Parse(txfechalimite.Text);
+                bd.insertarGastos(codigo, nombre, cantidad, fecha_creacion, fecha_limite);
+                carga();
+                limpiar();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script language='JavaScript'>alert('Error en la información...!!!');</script>");
+            }
+
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(txtid.Text);
+                int cuentaregistros = 0;
+                var registro = bd.buscarGastos(codigo);
+                foreach (var campo in registro)
+                {
+                    txtnombre.Text = campo.nombre;
+                    txtmonto.Text = campo.monto.ToString();
+                    txtfechacreacion.Text = campo.fecha_creacion.ToString();
+                    txfechalimite.Text = campo.fecha_limite.ToString();
+
+                    cuentaregistros = 1;
+                }
+                if (cuentaregistros == 0)
+                {
+                    Response.Write("<script>alert('id no Existe...!!!');</script>");
+                    limpiar();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Error en la información...!!!');</script>");
+            }
+
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(txtid.Text);
+                bd.eliminarGastos(codigo);
+                carga();
+                limpiar();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Error en la información...!!!');</script>");
+            }
+
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = int.Parse(txtid.Text);
+                String nombre = txtnombre.Text;
+                float cantidad = float.Parse(txtmonto.Text);
+
+                DateTime fecha_limite = DateTime.Parse(txfechalimite.Text);
+                bd.actualizarGastos(codigo, nombre, cantidad, fecha_limite);
+                carga();
+                limpiar();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Error en la información...!!!');</script>");
+            }
+        }
+    }
+}
---
+++ Formularios/Gasto.aspx.designer.cs
@@ -0,0 +1,179 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PaginaAhorro.Formularios
+{
+
+
+    public partial class Gasto
+    {
+
+        /// <summary>
+        /// Control Panel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Control txtid.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtid;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control txtnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombre;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control txtmonto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtmonto;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control txtfechacreacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfechacreacion;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control txfechalimite.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txfechalimite;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscar;
+
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
+
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control DatosGastos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView DatosGastos;
+
+        /// <summary>
+        /// Control LinqDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinqDataSource LinqDataSource1;
+    }
+}
